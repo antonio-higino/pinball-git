4a44b92d6b837ee2f870dc8b1df1cc49cc61fb71
Antonio Higino B L Medeiros <antoniobisneto98@gmail.com>
2022-05-30 22:41:28 -0300

    Reset da bola, contador de bolas e game over funcional
    Agora falta mostrar essas informações pro jogador.

---
+++ Assets/ControleBolas.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleBolas : MonoBehaviour
+{
+    Vector3 posicaoReset;
+    public int bolasRestantes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicaoReset = new Vector3(10.52f, 3.94f, -1.19f);
+        bolasRestantes = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.attachedRigidbody) {
+            if (bolasRestantes > 0)
+            {
+                //other.gameObject.transform.Translate(Vector3.forward * 0f * Time.deltaTime);
+                other.gameObject.transform.position = posicaoReset;
+                bolasRestantes--;
+                Debug.Log("Bolas restantes: " + bolasRestantes);
+            }
+            else {
+                Debug.Log("Game Over");
+            }
+        }
+    }
+}
